81fbe31f42e9ba38f2569c24d1d51cfef17ea8f1
andreisolo0 <solomonandrei2605@gmail.com>
2022-05-04 13:35:03 +0000

    Added to DigitalOcean and few minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: "admin", email: "admin@overwatch.com", password: "5y5t3mp455123456", admin: true, regular_user: false, viewer: false, first_name: "Overwatch", last_name: "ADMIN")
+Host.create(hostname: "overwatch.localhost", ip_address_or_fqdn: "127.0.0.1", user_id: 1,  user_to_connect: "root", password: "test", ssh_port: 22, run_as_sudo: false)
