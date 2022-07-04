b8010fa396354466a77f325b01b1948ea1e6d568
andreisolo0 <solomonandrei2605@gmail.com>
2022-07-04 13:34:52 +0000

    commit last changes and additions for Heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: "admin", email: "admin@overwatch.com", password: "UjvdB2Y7", admin: true, regular_user: false, viewer: false, first_name: "Overwatch", last_name: "ADMIN")
-Host.create(hostname: "overwatch.localhost", ip_address_or_fqdn: "127.0.0.1", user_id: 1,  user_to_connect: "root", password: "test", ssh_port: 22, run_as_sudo: false)
+User.create(username: "admin", email: "admin@overwatch.com", password: Rails.application.credentials.admin_pass , admin: true, regular_user: false, viewer: false, first_name: "Overwatch", last_name: "ADMIN")
+Host.create(hostname: "overwatch.localhost", ip_address_or_fqdn: "127.0.0.1", user_id: 1,  user_to_connect: "root", password: Rails.application.credentials.localhost_pass , ssh_port: 22, run_as_sudo: false)
