e5e5a43e80171453245d0609174eaa749a385ed6
Andrei Solomon <80392669+andreisolo0@users.noreply.github.com>
2022-07-04 17:04:27 +0300

    Update sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@ require 'sidekiq-scheduler'
 
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://:p6778c46d50b21e1c36106437d5aa3a889fcad51d556ad9aa7397fbdda2fcbdda@ec2-23-20-19-160.compute-1.amazonaws.com:7599" }
+  config.redis = { url: "redis://:REDACTED@ec2-23-20-19-160.compute-1.amazonaws.com:7599" }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: "redis://:p6778c46d50b21e1c36106437d5aa3a889fcad51d556ad9aa7397fbdda2fcbdda@ec2-23-20-19-160.compute-1.amazonaws.com:7599" }
+    config.redis = { url: "redis://:REDACTED@ec2-23-20-19-160.compute-1.amazonaws.com:7599" }
 end
 
 Sidekiq.configure_server do |config|
