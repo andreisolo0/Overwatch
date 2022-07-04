5e3ad6c9328eb70b8176b5f815327d990da5a8c8
andreisolo0 <solomonandrei2605@gmail.com>
2022-07-04 10:43:30 +0000

    latest

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "overwatch.solomon-andrei.com,overwatch-demo.herokuapp.com"
+  config.hosts << "overwatch-demo.herokuapp.com"
   #config.force_ssl = true
 
   
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,5 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.hosts << "overwatch-app.solomon-andrei.com"
+  #config.hosts << "overwatch-app.solomon-andrei.com"
+  config.hosts << "overwatch-demo.herokuapp.com"
 end
